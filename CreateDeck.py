8d1bbe00eb243abd111fbdb9bc486c20a338e7ce
jethet <henriette.hettinga@gmail.com>
2019-02-18 14:16:56 +0100

    Declare variables for game: suits, ranks and values

---
+++ CreateDeck.py
@@ -0,0 +1,10 @@
+import random
+
+# Global variables:
+suits = ('Spades', 'Hearts', 'Diamonds', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
+        'Ten', 'Ace', 'King', 'Queen', 'Jack')
+values: {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+         'Nine':9, 'Ten':10, 'Ace':11, 'King':10, 'Queen':10, 'Jack':10}
+
+# Create class cards with attributes suit, rank
