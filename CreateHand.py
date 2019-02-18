d8dd06bd9a7f43165e032130325709edbdd6ae1b
jethet <henriette.hettinga@gmail.com>
2019-02-18 15:46:21 +0100

    Create class Hand to keep card objects and calculate their value

---
+++ CreateHand.py
@@ -0,0 +1,7 @@
+# The Hand class holds the card objects that are dealt from the deck.
+# In addition, the Hand class can calculate the values of the cards using
+# the value dict as defined. The Hand class can adjust for the Ace (1 or 11).
+
+
+class Hand():
+    
