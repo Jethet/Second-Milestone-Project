4af7153c596f82247caef8251944690a3a41eed3
jethet <henriette.hettinga@gmail.com>
2019-02-19 16:01:22 +0100

    Create function for choice between hit or stand in game

--- FunctionsGame.py
+++ FunctionsGame.py
@@ -3,7 +3,6 @@
 def take_bet(chips):
 
     while True:
-
         try:
             chips.bet = int(input("How many chips do you want to bet? "))
         except:
@@ -19,3 +18,22 @@ def hit(deck, hand):
     single_card = deck.deal()
     hand.add_card(single_card)
     hand.adjust_for_ace()
+
+def hit_or_stand(deck,hand):
+    global playing   # to control the upcoming while loop
+    while True:
+        x = input("Hit or stand? Enter h or s. ")
+        if x[0].lower() == 'h':
+            hit(deck,hand)
+        elif x[0].lower() == 's':
+            print("Player Stands. Dealer's turn.")
+            playing = False
+
+        else:
+            print("Please enter h or s only.")
+            continue
+        break
+
+
+
+#
