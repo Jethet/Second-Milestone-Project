454952e2c92cfcc58c564f507091ac08c8655ef5
jethet <henriette.hettinga@gmail.com>
2019-02-19 14:36:08 +0100

    Correct values and integrate code in one file

--- CreateDeck.py
+++ CreateDeck.py
@@ -4,7 +4,7 @@ import random
 suits = ('Spades', 'Hearts', 'Diamonds', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
         'Ten', 'Ace', 'King', 'Queen', 'Jack')
-values: {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
          'Nine':9, 'Ten':10, 'Ace':11, 'King':10, 'Queen':10, 'Jack':10}
 
 playing = True
@@ -49,6 +49,33 @@ class Deck():
 #test_deck = Deck()
 #test_deck.shuffle()
 #print(test_deck)
+
+# CREATE HAND code
+
+class Hand:
+
+    def __init__(self):
+        self.cards =[]   # start with an empty list
+        self.value = 0   # start with zero value
+        self.aces = 0    # at an attribute to keep track of aces
+
+    def add_card(self, card):
+        # card is passed in from Deck.deal() ---> single Card(suit,rank)
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+    def adjust_for_ace(self):
+        pass
+
+test_deck = Deck()
+test_deck.shuffle()
+# create player:
+test_player = Hand()
+# deal 1 card from deck: CARD(suit,rank)
+pulled_card = test_deck.deal()
+print(pulled_card)
+test_player.add_card(pulled_card)
+print(test_player.value)
 
 
 
